b5b6c6eb6fa30b4b6fdf277dae28c0f2332f6312
Niteshbabu21 <niteshpalutla@gmail.com>
2025-08-19 19:27:46 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class code {
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the two numbers : ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = a+b;
+    System.out.println("Addition is : "+c);
+    
+
+
+  }
+}
